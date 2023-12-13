2ba30e7e774926073c01eda84965192f9e97e1c5
Alauddin-24434 <muhammadalauddin24434@gmail.com>
2023-12-13 17:48:44 +0600

    implemnt messanger chat inside my resturent website

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import { HelmetProvider } from 'react-helmet-async'
 import AuthProvider from './providers/AuthProvider'
-import {  QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { router } from './routes/Routes'
@@ -19,11 +19,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <HelmetProvider>
 
     <AuthProvider>
-     <QueryClientProvider client={queryClient}>
-     <Toaster />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <RouterProvider router={router} />
 
-     </QueryClientProvider>
+       
+      </QueryClientProvider>
     </AuthProvider>
   </HelmetProvider>
+
 )
+
